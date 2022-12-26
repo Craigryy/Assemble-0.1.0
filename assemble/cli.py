38637b0900349a4a39b79fd6aba0fc492ac6f535
James Harrison <116971272+Craigryy@users.noreply.github.com>
2022-12-26 23:29:48 +0100

    updated the initial description column .
    replacing the description columns with the assigned title, notes and label columns.

--- assemble/cli.py
+++ assemble/cli.py
@@ -5,14 +5,13 @@ import typer
 from .managers import noteBookManager
 from .assemble import get_table
 
-
-app = typer.Typer(help="Command line tool to keep logs.")
+app = typer.Typer(help="Command line tool to keep notes.")
 
 
 @app.command(name='list')
 def list_log_entries():
     """
-    List all log entries in a table, limits upto 40 log entries.
+    List all log entries in a table, limits up to 10 log entries.
     """
     manager = noteBookManager()
     note_entries = manager.list()
@@ -22,7 +21,7 @@ def list_log_entries():
     else:
         typer.echo(
             typer.style(
-                f'You do not have any entries in your log book.',
+                f'You do not have any entries in your note book.',
                 fg=typer.colors.MAGENTA,
                 bold=True
             )
@@ -31,16 +30,25 @@ def list_log_entries():
 
 @app.command()
 def find(
-    description_contains: str = typer.Argument(
-        ...,
-        help="String that may match log entry description"
-    )
+        title: str = typer.Argument(
+            ...,
+            help="String that may match title entry description"
+        ),
+        notes: str = typer.Argument(
+            ...,
+            help="string that may match notes entry description"
+
+        ),
+        label: str = typer.Argument(
+            ...,
+            help="String that may match label entry description"
+        )
 ):
     """
     List all log entries that match the argument.
     """
     manager = noteBookManager()
-    note_entries = manager.find(description_contains)
+    note_entries = manager.find(title, notes, label)
 
     if note_entries:
         typer.echo(get_table(note_entries))
@@ -48,7 +56,7 @@ def find(
         typer.echo(
             typer.style(
                 f'You do not have any entries matching '
-                f'"{description_contains}" in your log book.',
+                f'"{title, notes, label}" in your log book.',
                 fg=typer.colors.RED,
                 bold=True
             )
@@ -57,50 +65,50 @@ def find(
 
 @app.command()
 def view(
-    id: int = typer.Argument(
-        ...,
-        help="ID of the log entry"
-    )
+        id: int = typer.Argument(
+            ...,
+            help="ID of the Note entry"
+        )
 ):
     """
-    View a single log entry using it's ID.
+    View a single note entry using it's ID.
     """
     manager = noteBookManager()
     note_entry = manager.get(id)
 
     if note_entry:
         note_entry_id = (
-            typer.style("Log Entry ID: ", fg=typer.colors.BRIGHT_BLUE, bold=True) +
-            str(note_entry.id)
+                typer.style("Note Entry ID: ", fg=typer.colors.BRIGHT_BLUE, bold=True) +
+                str(note_entry.id)
         )
         typer.echo(note_entry_id)
 
         note_datetime = (
-            typer.style("Log Date & Time: ", fg=typer.colors.BRIGHT_BLUE, bold=True) +
-            note_entry.log_datetime.strftime("%Y-%m-%d %I:%M %p")
+                typer.style("Note Date & Time: ", fg=typer.colors.BRIGHT_BLUE, bold=True) +
+                note_entry.log_datetime.strftime("%Y-%m-%d %I:%M %p")
         )
         typer.echo(note_datetime)
 
         typer.echo(
-            typer.style("\nDescription:\n", fg=typer.colors.BRIGHT_BLUE, bold=True)
+            typer.style("\nTitle:\n", fg=typer.colors.BRIGHT_BLUE, bold=True)
         )
-        typer.echo(note_entry.description + '\n')
+        typer.echo(note_entry.title + '\n')
 
         created_at = (
-            typer.style("Created at: ", fg=typer.colors.BRIGHT_BLUE, bold=True) +
-            note_entry.created_at.strftime("%Y-%m-%d %I:%M %p")
+                typer.style("Created at: ", fg=typer.colors.BRIGHT_BLUE, bold=True) +
+                note_entry.created_at.strftime("%Y-%m-%d %I:%M %p")
         )
         typer.echo(created_at)
 
         updated_at = (
-            typer.style("Updated at: ", fg=typer.colors.BRIGHT_BLUE, bold=True) +
-            note_entry.updated_at.strftime("%Y-%m-%d %I:%M %p")
+                typer.style("Updated at: ", fg=typer.colors.BRIGHT_BLUE, bold=True) +
+                note_entry.updated_at.strftime("%Y-%m-%d %I:%M %p")
         )
         typer.echo(updated_at)
     else:
         typer.echo(
             typer.style(
-                f'No Log Entry Found with id={id}',
+                f'No Note Entry Found with id={id}',
                 fg=typer.colors.RED,
                 bold=True
             )
@@ -109,19 +117,27 @@ def view(
 
 @app.command()
 def add(
-    description: str = typer.Argument(
-        ...,
-        help="Description of the log entry"
-    ),
-    date: datetime = typer.Option(
-        datetime.now().strftime("%Y-%m-%d"), '--date', '-d',
-        help="Date of the log entry"
-    ),
-    time: datetime = typer.Option(
-        datetime.now().strftime("%I:%M %p"), '--time', '-t',
-        formats=["%H:%M:%S", "%I:%M %p"],
-        help="Time of the log entry"
-    )
+        title: str = typer.Argument(
+            ...,
+            help="Description of the log entry"
+        ),
+        notes: str = typer.Argument(
+            ...,
+            help="Description of the note entry"
+        ),
+        label: str = typer.Argument(
+            ...,
+            help="Label of the note entry"
+        ),
+        date: datetime = typer.Option(
+            datetime.now().strftime("%Y-%m-%d"), '--date', '-d',
+            help="Date of the log entry"
+        ),
+        time: datetime = typer.Option(
+            datetime.now().strftime("%I:%M %p"), '--time', '-t',
+            formats=["%H:%M:%S", "%I:%M %p"],
+            help="Time of the log entry"
+        )
 ):
     """
     Add a log entry to the logbook.
@@ -130,7 +146,7 @@ def add(
     note_datetime = datetime.combine(date, note_entry_time)
 
     manager = noteBookManager()
-    created, message = manager.create(description, note_datetime)
+    created, message = manager.create(title, notes, label, note_datetime)
 
     if created:
         typer.echo(
@@ -144,23 +160,31 @@ def add(
 
 @app.command()
 def edit(
-    id: int = typer.Argument(
-        ...,
-        help="ID of the log entry"
-    ),
-    description: str = typer.Option(
-        "", '--description',
-        help="New Description for the log entry"
-    ),
-    date: datetime = typer.Option(
-        None, '--date', '-d',
-        help="New Date for the log entry"
-    ),
-    time: datetime = typer.Option(
-        None, '--time', '-t',
-        formats=["%H:%M:%S", "%I:%M %p"],
-        help="New Time for the log entry"
-    )
+        id: int = typer.Argument(
+            ...,
+            help="ID of the log entry"
+        ),
+        title: str = typer.Option(
+            "", '--description',
+            help="New Description for the log entry"
+        ),
+        notes: str = typer.Option(
+            "", '--description',
+            help="New Description for the log entry"
+        ),
+        label: str = typer.Option(
+            "", '--description',
+            help="New Description for the log entry"
+        ),
+        date: datetime = typer.Option(
+            None, '--date', '-d',
+            help="New Date for the log entry"
+        ),
+        time: datetime = typer.Option(
+            None, '--time', '-t',
+            formats=["%H:%M:%S", "%I:%M %p"],
+            help="New Time for the log entry"
+        )
 ):
     """
     Update a log entry using it's ID.
@@ -168,13 +192,15 @@ def edit(
     note_datetime = None
 
     if date and time:
-        log_entry_time = time.time()
-        log_datetime = datetime.combine(date, log_entry_time)
+        note_entry_time = time.time()
+        note_datetime = datetime.combine(date, note_entry_time)
 
     manager = noteBookManager()
     updated, message = manager.update(
         id,
-        description=description,
+        title=title,
+        notes=notes,
+        label=label,
         note_datetime=note_datetime
     )
 
@@ -190,13 +216,13 @@ def edit(
 
 @app.command()
 def delete(
-    id: int = typer.Argument(
-        ...,
-        help="ID of the note entry"
-    )
+        id: int = typer.Argument(
+            ...,
+            help="ID of the log entry"
+        )
 ):
     """
-    Delete a note entry using it's ID.
+    Delete a log entry using it's ID.
     """
     manager = noteBookManager()
     deleted, message = manager.delete(id)
@@ -209,3 +235,4 @@ def delete(
         typer.echo(
             typer.style(message, fg=typer.colors.RED, bold=True)
         )
+
