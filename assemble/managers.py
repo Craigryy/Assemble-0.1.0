05f3dba991be167de7da20049585beeebe45e0bb
James Harrison <116971272+Craigryy@users.noreply.github.com>
2022-12-27 22:28:55 +0100

    Update managers.py

--- assemble/managers.py
+++ assemble/managers.py
@@ -7,13 +7,18 @@ from .assemble import get_database_url
 
 class noteBookManager:
     model = noteBook
-
+    
+  #Create engine that will allow us to communicate with database.
+#Creating session which is the middle ground to talk to our engine.
+#Define structure of table
     def __int__(self):
         self.engine = create_engine(get_database_url(), echo=False)
         self.session = sessionmaker(bind=self.engine)()
-
+       
         if not inspect(self.engine).has_table(self.model.__tablename__):
             self.model.metadata.create_all(bind=self.engine)
+            
+            
 
     def list(self):
         return self.session.query(self.model).order_by(
@@ -27,16 +32,29 @@ class noteBookManager:
             self.model.note_datetime.desc()
         ).all()
 
-    def get(self,identity ):
+    def get(self,identity):
         return self.session.query(self.model).get(identity)
 
     def create(self, title, notes, label, note_datetime):
         note_entry = self.model(
             title=title,
             notes=notes,
-            label=label,
+            label=label
             note_datetime=note_datetime
         )
+         file_name = "folder app -notesapp csv.csv"
+        data = Load_Data(file_name)
+
+        for i in data:
+            record = noteBook(**{
+                'date': datetime.strptime(i[5], '%d-%b-%y').date(),
+                'id': i[0],
+                'title': i[2],
+                'notes': i[3],
+                'label': i[4],
+            })
+            s.add(record)  # Add all the records
+            
         try:
             self.session.add(note_entry)
             self.session.commit()
@@ -49,7 +67,7 @@ class noteBookManager:
         if title or notes or label or note_datetime:
             return False, 'you must provide details.'
 
-            note_entry = self.session.query(self.model).get(identity)
+            note_entry = self.session.query(self.model).get(identity0)
             
                 try:
                     self.session.commit()
@@ -60,8 +78,8 @@ class noteBookManager:
                 else:
                     return False
 
-    def delete(self, identity):
-        delete_count = self.session.query(self.model).filter_by(identity=identity).delete()
+    def delete(self, title):
+        delete_count = self.session.query(self.model).filter_by(title=title).delete()
 
         if delete_count > 0:
             try:
@@ -70,3 +88,5 @@ class noteBookManager:
             except Exception as e:
                 self.session.rollback()
                 return (False, f'Oops an error ocurred while deleting a note.{e}')
+            else:
+                return (False, f'Oops no Note Entryfound with title={title}')
