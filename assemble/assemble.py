35d6e1953fd4112d58caf91c131ffa867d3b0197
James Harrison <116971272+Craigryy@users.noreply.github.com>
2022-12-27 19:46:35 +0100

    Update assemble.py

--- assemble/assemble.py
+++ assemble/assemble.py
@@ -5,11 +5,13 @@ from tabulate import tabulate
 
 
 def get_table(note_entries):
-    """Create table for Log Entries using tabulate"""
+    """Create table for Note Entries using tabulate"""
     table_data = [
         [
             entry.id,
-            entry.description[0:30],
+            entry.title[0:30],
+            entry.notes[0:30],
+            entry.label[0:30], 
             entry.log_datetime.strftime("%Y-%m-%d %I:%M %p"),
             entry.created_at.strftime("%Y-%m-%d %I:%M %p"),
             entry.updated_at.strftime("%Y-%m-%d %I:%M %p")
@@ -19,7 +21,9 @@ def get_table(note_entries):
 
     table_headers = [
         "# ID",
-        "description",
+        "title",
+        "notes",
+        "label",
         "Log Datetime",
         "Created At",
         "Updated At"
@@ -34,10 +38,10 @@ def get_table(note_entries):
 
 def get_database_url():
     """Get Database URL from environment variable or "~/.notebook/" directory"""
-    NOTE_BOOK_DATABASE_URL = os.environ.get('NOTE_BOOK_DATABASE_URL')
+    note_book_data_url = os.environ.get('NOTE_BOOK_DATABASE_URL')
 
-    if NOTE_BOOK_DATABASE_URL:
-        return NOTE_BOOK_DATABASE_URL
+    if note_book_data_url:
+        return note_book_data_url
 
     database_directory = str(Path.home() / '.notebook')
 
