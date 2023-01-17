71363d977f52124714da879bcd85015196a5cd1c
James Harrison <116971272+Craigryy@users.noreply.github.com>
2023-01-17 12:26:41 +0100

    updated and completed

--- assemble/models.py
+++ assemble/models.py
@@ -10,24 +10,28 @@ class Folder(Base):
 
     __tablename__ = "folders"
 
-    gid = Column(Integer(), primary_key=True)
+    id = Column(Integer(), primary_key=True)
     name = Column(String(200), nullable=False)
+    files = relationship(
+        "File",
+        cascade="all, delete-orphan",
+        back_populates="folder",
+        lazy="selectin",
+    )
 
-    files = relationship("File", back_populate='author', lazy='noload', cascade='all,delete')
-
-    def __init__(self, name, notes, gid):
-        self.name = name
-        self.notes = notes
-        self.gid = gid
-
+    def __init__(self,name):
+        self.name=name
 
 class File(Base):
     __tablename__ = "files"
 
     id = Column(Integer(), primary_key=True, index=True)
-    title = Column(String(200), nullable=False)
-    notes = Column(String(200), nullable=False)
-    label = Column(String(200), nullable=False)
-
-    folder_gid = Column(String(20), ForeignKey('folders_gid'))
-    author = relationship("Folder", back_populate="files")
+    Title = Column(String(200), nullable=False)
+    Notes = Column(String(200), nullable=False)
+    Label = Column(String(20), nullable=False)
+    folder_id = Column(Integer(), ForeignKey("folders.id")
+    )
+    folder = relationship(
+        "Folder",
+        back_populates="files",
+    )
