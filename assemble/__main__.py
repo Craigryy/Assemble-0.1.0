299918570a0c051d8981bde5cb19449da39231e4
James Harrison <116971272+Craigryy@users.noreply.github.com>
2023-01-24 11:28:57 +0100

    Update __main__.py

--- assemble/__main__.py
+++ assemble/__main__.py
@@ -1,12 +1,12 @@
 import typer
 from rich.traceback import install
-from assemble import files,folder
+from assemble import files, folder
 
-app=typer.Typer()
-app.add_typer(files.app , name="File")
+app = typer.Typer()
+""" Sub-nested commands for typer Scripts."""
+app.add_typer(files.app, name="File")
 app.add_typer(folder.app, name='Folder')
 install()
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app()
