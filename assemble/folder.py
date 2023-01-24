5e55228f0f8c1c925e9f92bdd2c7b8b599765c0d
James Harrison <116971272+Craigryy@users.noreply.github.com>
2023-01-24 11:55:49 +0100

    updated folder.py

--- assemble/folder.py
+++ assemble/folder.py
@@ -8,8 +8,8 @@ app = typer.Typer(help='A command line tool to keep your folders')
 @app.command()
 def listFolders():
     """
-       List all folders in a table.
-       """
+    List all Folder entries in a table, limits up to 40 Folder entries.
+    """
     manager = folderManager()
     folder_entries = manager.list()
 
@@ -24,22 +24,32 @@ def listFolders():
             )
         )
 
-@app.command()
-def add(name,notes):
-        """
-        Add a folder entry.
-        """
-        manager = folderManager()
-        created, message = manager.addFolder(name,notes)
 
-        if created:
-            typer.echo(
-                typer.style(message, fg=typer.colors.GREEN, bold=True)
-            )
-        else:
-            typer.echo(
-                typer.style(message, fg=typer.colors.RED, bold=True)
-            )
+@app.command()
+def add(
+        name: str = typer.Argument(
+            ...,
+            help="Name of the folder entry."
+        )
+        , notes: str = typer.Argument(
+            ...,
+            help="Note of a folder entry."
+        )
+):
+    """
+    Add a folder entry.
+    """
+    manager = folderManager()
+    created, message = manager.addFolder(name, notes)
+
+    if created:
+        typer.echo(
+            typer.style(message, fg=typer.colors.GREEN, bold=True)
+        )
+    else:
+        typer.echo(
+            typer.style(message, fg=typer.colors.RED, bold=True)
+        )
 
 
 @app.command()
@@ -47,12 +57,11 @@ def delete(
         id: int = typer.Argument(
             ...,
             help="ID of a folder entry"
-    )
+        )
 ):
     """
-           Delete a folder  entry.
-           """
-
+    Delete a folder entry using its ID.
+    """
     manager = folderManager()
     deleted, message = manager.delete(id)
     if deleted:
@@ -63,6 +72,7 @@ def delete(
         typer.echo(
             typer.style(message, fg=typer.colors.RED, bold=True)
         )
+
 
 if __name__ == "__main__":
     app()
