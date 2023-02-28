76c558e4c690b485b70f33107b5a7b8d40cc9198
James Harrison <116971272+Craigryy@users.noreply.github.com>
2023-02-28 21:00:58 +0100

    added a doc.py file to generate a markdown documentation for crud operations

---
+++ assemble/docs.py
@@ -0,0 +1,44 @@
+import typer
+
+
+app = typer.Typer(help="Generate a markdown documentation")
+
+
+@app.command()
+def Docs(folder: str, file: str, one_to_many: bool = True):
+    """
+    Generate Markdown documentation for CRUD operation of a folder and child in one-many relationship .
+    """
+    title = f"{folder.title()} and {file.title()} CRUD Documentation"
+    print(f"#{title}\n")
+
+    if one_to_many:
+        print(f"## {folder.title()}\n")
+        print(f"### Create {folder.title()}\n")
+        print(f"### Request\n")
+        print(f"json\n{{\n    \"id\": \"integer\",\n    \"name\": \"string\", \"notes\":\"string\"\n}}\n")
+        print(f"### Get {folder.title()}\n")
+        print(f"#### Request\n")
+        print(f"http\nGET /{folder.lower()}s/{{id}}\n")
+        print(f"#### Response\n")
+        print(
+            f"json\n{{\n    \"id\": \"integer\",\n    \"name\": \"string\",\n    \"{file.lower()}s\": [\n              {{\n       \"id\": \"integer\",\n            \"title\": \"string\",\n    \"notes\": \"string\",\n   \"label\": \"string\",\n       }}\n    ]\n}}\n")
+
+        print(f"## {file.title()}\n")
+        print(f"### Create {file.title()}\n")
+        print(f"#### Request\n")
+        print(f"json\n{{\n    \"title\": \"string\",\n   \"notes\": \"string\",\n   \"label\": \"string\"\n,    \"{folder.lower()}_id\": \"integer\"\n}}\n")
+        print(f"#### Response\n")
+        print(
+            f"json\n{{\n    \"id\": \"integer\",\n   \"title\": \"string\",\n   \"notes\": \"string\",\n    \"label\": \"string\",\n     \"{folder.lower()}_id\": \"integer\"\n}}\n")
+        print(f"### Get {file.title()}\n")
+        print(f"#### Request\n")
+        print(f"http\nGET /{file.lower()}s/{{id}}\n")
+        print(f"#### Response\n")
+        print(
+            f"json\n{{\n    \"id\": \"integer\",\n    \"title\": \"string\",\n       \"notes\": \"string\",\n    \"label\": \"string\",\n      \"{folder.lower()}_id\": \"integer\",\n    \"{folder.lower()}\": {{\n        \"id\": \"integer\",\n        \"name\": \"string\"\n    }}\n}}\n")
+
+    else:
+        # Code for many-to-many relationship CRUD documentation
+        pass
+
