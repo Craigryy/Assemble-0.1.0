ee8d55900e7c6ad39a757c160cde3418ae53474b
James Harrison <116971272+Craigryy@users.noreply.github.com>
2023-03-07 17:04:23 +0100

    updated and implemented the def sav functionality.

--- assemble/files.py
+++ assemble/files.py
@@ -1,10 +1,9 @@
 import typer
 import csv
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from assemble.models import File
-from assemble.manager import fileManger
-from assemble.database import get_file_table,get_database_url
+from assemble.models import File,Folder
+from assemble.manager import fileManger,folderManager
+from assemble.database import get_file_table
+from typing import Optional
 
 
 app = typer.Typer(help="Save your files to a folder ")
@@ -79,35 +78,66 @@ def List():
             )
         )
 
-
 @app.command()
-def Add(ctitle, cnotes, clabel):
+def Add(ctitle, cnotes ,clabel: Optional[str] = typer.Argument(None)):
     """
     Add a file entry to a folder.
     """
-    manager = fileManger()
-    created, message = manager.addFile(ctitle, cnotes, clabel)
+    manager= fileManger() or folderManager()
 
-    if created:
-         typer.echo(
-             typer.style(message, fg=typer.colors.GREEN, bold=True)
-         )
+    if clabel is None:
+        clabel="default"
+        # query database for a default
+        default_folder= manager.session.query(Folder).filter(Folder.name=="default_folder").first()
+        if default_folder:
+            # #create a file
+            file = File(title=ctitle, notes=cnotes,label=clabel, author=default_folder)
+            manager.save(file)
+            typer.echo(typer.style( f"Default folder created and file added to folder name: {default_folder.name}. ",fg=typer.colors.MAGENTA,bold=True))
+        else:
+            default_f = Folder(name="default", notes="have a nice day")
+            manager.session.add(default_f)
+            child = File(title=ctitle, notes=cnotes, label=clabel, author=default_f)
+            manger.save(child)
+            typer.echo(typer.style(f"Default folder created and file  to folder name: {default_f.name}. ",
+                                   fg=typer.colors.MAGENTA, bold=True))
+
+
+    elif clabel is not None:
+        # query the database folder that matches the file
+        file_folder = manager.session.query(Folder).filter(Folder.name == clabel).first()
+        # create a new file
+        NewFile = File(title=ctitle, notes=cnotes, label=clabel, author=file_folder)
+        manager.save(NewFile)
+        typer.echo(typer.style(f"file added to an existing folder name:{NewFile.label}",fg=typer.colors.MAGENTA,bold=True))
+
+
     else:
+        #create a new folder.
+        new_folder = Folder(name=clabel,notes="have a nice day")
+        manager.session.add(new_folder)
+        # craete a new file
+        new_file= File(title=ctitle,notes=cnotes,clabel=clabel,author=new_folder)
+        manager.save(new_file)
         typer.echo(
-            typer.style(message, fg=typer.colors.RED, bold=True)
-        )
+            typer.style(f"a new file created and added to an existing folder name:{new_folder.name}", fg=typer.colors.MAGENTA, bold=True))
+        return
 
 
+
+@app.command()
 def Edit(title, notes, label):
     """
     Update a file entry using its label.
     """
+
     manager = fileManger()
     updated, message = manager.update(
         title=title,
         notes=notes,
         label=label
     )
+
     if updated:
         typer.echo(
             typer.style(message, fg=typer.colors.GREEN, bold=True)
@@ -127,39 +157,34 @@ def Delete(
     """
     Delete a folder entry using its name and all of its file(s).
     """
-    engine=create_engine(get_database_url())
-    Session=sessionmaker(bind=engine)
-    session=Session()
 
-    filee = session.query(File).filter(File.title==title).first()
+    manager = fileManger()
+
+    filee = manager.session.query(File).filter(File.title==title).first()
     if not filee:
         typer.echo(typer.style(f"No file with title {title} found in the database.",fg=typer.colors.RED,bold=True))
         return
 
-    file = session.query(File).filter(File.title==title).first()
+    file = manager.session.query(File).filter(File.title==title).first()
 
     if not yes:
         #prompt the user to confirm the deletion.
-        confirm= typer.confirm(typer.style(f"Are you sure you want to delete file title: {title} and all of its files found in the database.",fg=typer.colors.MAGENTA,bold=True))
+        confirm= typer.confirm(typer.style(f"Are you sure you want to delete file title: {title}.",fg=typer.colors.MAGENTA,bold=True))
         if not confirm:
             return
 
-
-    session.delete(file)
-    session.delete(filee)
-    session.commit()
+    manager.session.delete(filee)
+    manager.session.delete(file)
+    manager.session.commit()
 
     typer.echo(typer.style(f"File {title} have been deleted .",fg=typer.colors.GREEN,bold=True))
-
 
 @app.command()
 def insert(csv_filename: str):
     """
     insert csv into model database
     """
-    engine = create_engine(get_database_url())
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    manager=fileManger()
 
     # Open the CSV file and insert its contents into the table
     with open(csv_filename) as csvfile:
@@ -170,21 +195,14 @@ def insert(csv_filename: str):
                         notes=row[1],
                         label=row[2]
                         )
-            session.add(data)
-            session.commit()
+            manager.save(data)
 
     typer.echo(typer.style(f"csv file added to file table .",fg=typer.colors.GREEN,bold=True))
 
 
+
 if __name__ == "_main_":
     app()
-
-
-
-
-
-
-
 
 
 
